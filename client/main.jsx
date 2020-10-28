4fafe03b43292a11cd48029a7bae8d142b655554
Kate <nikaterina@gmail.com>
2020-10-28 23:19:10 +0300

    webpack babel eslint

---
+++ client/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './main.scss'
+
+const Home = () => <div>12345678</div>
+
+const target = document.getElementById('root')
+
+ReactDOM.render(<Home />, target)
